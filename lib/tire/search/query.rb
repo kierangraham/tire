50d3b49dea3410d2e8996fd44a113777d10c2e5f
Kieran Graham <me@kierangraham.com>
2012-07-12 20:15:36 +0100

    fixing custom score implementation again

--- lib/tire/search/query.rb
+++ lib/tire/search/query.rb
@@ -45,7 +45,8 @@ module Tire
       def custom_score(options={}, &block)
         @custom_score ||= Query.new(&block)
         @value[:custom_score] = {}
-        @value[:custom_score].update({:query => @custom_score.to_hash.merge(options)})
+        @value[:custom_score].update({:query => @custom_score.to_hash})
+        @value[:custom_score].update(options)
         @value
       end
 
